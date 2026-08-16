b031012cb7a05ce55cc9a0e104bb130bec31d6fb
Reece Turner <reeceturner1358@gmail.com>
2026-08-15 23:52:03 -0400

    still needs testing

---
+++ lazyscript.pm
@@ -0,0 +1,93 @@
+
+package LazyScript;
+
+use strict;
+use warnings;
+use feature 'say';
+
+my @tokens = ();
+
+# argue token to push
+sub add_to_tokens {
+    my $itemToPush = shift;
+    push(@tokens, $itemToPush);
+}
+
+# argue file to lex
+sub lex {
+    my $fileToLex = shift;
+    open(my $fh, '<', $fileToLex) or die 'Failed to open ' . $fileToLex . ' Are you sure it exists?';
+    while (my $line = <$fh>) {
+        if ($line =~ /let\s(?<variable>\w+)\s=\slisten/) {
+            add_to_tokens({
+                type => 'assigned_listen',
+                assign_to => $+{variable} 
+            });
+        } elsif ($line =~ /listen/) {
+            add_to_tokens({
+                type => 'plain_listen'
+            });
+        } elsif ($line =~ /speak\s(?<output>\w+)/) {
+            add_to_tokens({
+                type => 'speak',
+                output => $+{output}
+            });
+        }
+    }
+    close $fh;
+
+    @tokens == 0 and die 'Failed to locate any LazyScript keywords in the file ' . $fileToLex;
+}
+
+my @snippets = ();
+
+# argue snippet to push
+sub pushSnippet {
+    my $snippetToPush = shift;
+    push(@snippets, $snippetToPush);
+}
+
+sub parse {
+    for my $token (@tokens) {
+        my $type = $token->{'type'};
+
+        if ($type eq 'assigned_listen') {
+            pushSnippet(qq{
+            const $token->{'assign_to'} = await rl.question('');
+            rl.close();
+            });
+        } elsif ($type eq 'plain_listen') {
+            pushSnippet(qq{
+            await rl.question('');
+            });
+        } elsif ($type eq 'speak') {
+            pushSnippet(qq{
+            console.log($token->{'output'});
+            });
+        }
+    }
+}
+
+# argue the name of the output file 
+sub generate {
+    my $outputFileName = shift;
+
+    my $readlineBoilerplate = qq{
+    import readline from 'node:readline/promises';
+    import { stdin as input, stdout as output } from 'node:process';
+
+    const rl = readline.createInterface({ input, output });
+    };
+
+    die 'An internal action failed: parser' if @snippets == 0;
+
+    open(my $newFh, '>', $outputFileName);
+    print $newFh $readlineBoilerplate;
+    for my $snippet (@snippets) {
+        print $newFh $snippet;
+    }
+    close $newFh;
+    return $outputFileName
+}
+
+1;
---
+++ lazyscript_cli.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/env perl
+
+use strict; use warnings;
+use feature 'say';
+use FindBin;
+use lib "$FindBin::Bin";
+use LazyScript;
+
+my @arguments = @ARGV;
+
+my $command = $arguments[0];
+
+sub transpile {
+    my $fileGiven = $arguments[1];
+    my $fileGoingOut = "$fileGiven.js";
+
+    LazyScript::lex($fileGiven);
+    LazyScript::parse();
+    LazyScript::generate($fileGoingOut);
+
+    say 'There you go. The new file is titled ' . $fileGoingOut . ' Enjoy.'; 
+}
+
+if (grep {$_ eq $command} qw|transpile digest render|) {
+    transpile();
+}
